3430c36961d2d53cec05cd5e5a85dda4fa365755
wanggang <wanggang225@163.com>
2023-03-27 22:55:25 +0800

    添加vue-router@4支持

--- src/main.js
+++ src/main.js
@@ -10,7 +10,10 @@ import './theme.css'
 
 import { createPinia } from 'pinia'
 
+import router from './router'
+
 createApp(App)
   .use(TDesign)
   .use(createPinia())
+  .use(router)
   .mount('#app')
